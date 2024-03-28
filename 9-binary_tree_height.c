bb6cc10990ce20c214101494921704b6649442d9
zscbana <zscbana@gmail.com>
2024-03-28 08:15:00 +0200

    9.1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * @brief
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
  *
+ * Return: 0 if the tree is NULL
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -10,10 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t left = 0, right = 0;
+
 		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((left > right) ? left : right);
 	}
 
-	return 0;
+	return (0);
 }
