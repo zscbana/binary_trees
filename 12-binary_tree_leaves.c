abd8f43e5c6c8b9f3be7736351bd9b899408ce27
zscbana <zscbana@gmail.com>
2024-03-28 08:26:42 +0200

    12.1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,8 +15,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		if (!tree->left && !tree->right)
 			leaves++;
-		binary_tree_leaves(tree->left);
-		binary_tree_leaves(tree->right);
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
 	}
 	return (leaves);
 }
