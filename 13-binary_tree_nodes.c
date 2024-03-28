0e0713dc91ae07e8ffb2f02c50083027a3145258
zscbana <zscbana@gmail.com>
2024-03-28 08:31:31 +0200

    13.1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		if (tree->left->parent || tree->right->parent)
+		if (tree->left || tree->right)
 			nodes++;
 		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
